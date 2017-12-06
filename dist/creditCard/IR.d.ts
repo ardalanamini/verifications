cdf04b61d677dcb022e2d08fd39dfd9b8e0155ac
Ardalan Amini <ardalan1074@gmail.com>
2017-12-06 22:02:03 +0330

    Initial commit

--- dist/creditCard/IR.d.ts
+++ dist/creditCard/IR.d.ts
@@ -1,6 +1,15 @@
+export interface Bank {
+    name: string;
+    alias: string;
+    website: string;
+}
 /**
  *
  * @see http://www.aliarash.com/article/creditcart/credit-debit-cart.htm
  */
 export declare function verify(code: string): boolean;
-export declare function identify(code: string): void;
+export declare function identify(code: string): {
+    valid: boolean;
+    type: string | undefined;
+    bank: Bank | undefined;
+};
--- dist/creditCard/IR.js
+++ dist/creditCard/IR.js
@@ -1,28 +1,128 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const banks = {
+    '02229': {
+        name: 'Bank Pasargad',
+        alias: 'BPI',
+        website: 'https://www.bpi.ir'
+    },
+    '02806': {
+        name: 'Shahr Bank',
+        alias: 'Shahr Bank',
+        website: 'http://shahr-bank.ir'
+    },
+    '02908': {
+        name: 'Tose\'e Ta\'avon Bank',
+        alias: 'Tose\'e Ta\'avon Bank',
+        website: 'http://www.ttbank.ir'
+    },
+    '02938': {
+        name: 'Bank Day',
+        alias: 'Bank Day',
+        website: 'http://www.bank-day.ir/'
+    },
+    '03769': {
+        name: 'Bank Saderat Iran',
+        alias: 'BSI',
+        website: 'http://www.bsi.ir/'
+    },
+    '03770': {
+        name: 'Keshavarzi Bank',
+        alias: 'BKI',
+        website: 'http://www.bki.ir'
+    },
     '03799': {
         name: 'Bank Melli Iran',
         alias: 'BMI',
         website: 'https://bmi.ir'
     },
-    '89210': {
-        name: 'Bank Sepah',
-        alias: 'BS',
-        website: 'https://www.ebanksepah.ir'
+    '10433': {
+        name: 'Bank Mellat',
+        alias: 'Bank Mellat',
+        website: 'https://www.bankmellat.ir'
+    },
+    '21986': {
+        name: 'Saman Bank',
+        alias: 'SB',
+        website: 'https://www.sb24.com'
+    },
+    '22106': {
+        name: 'Parsian Bank',
+        alias: 'Parsian Bank',
+        website: 'http://www.persian-bank.ir'
+    },
+    '27353': {
+        name: 'Tejarat Bank',
+        alias: 'Tejarat Bank',
+        website: 'https://www.tejaratbank.ir'
+    },
+    '27381': {
+        name: 'Ansar Bank',
+        alias: 'Ansar Bank',
+        website: 'https://www.ansarbank.com'
+    },
+    '27412': {
+        name: 'Eghtesad Novin Bank',
+        alias: 'EN Bank',
+        website: 'https://www.enbank.ir'
+    },
+    '27488': {
+        name: 'Karafarin Bank',
+        alias: 'Karafarin Bank',
+        website: 'http://www.karafarinbank.ir'
     },
     '27648': {
         name: 'Export Development Bank of Iran',
         alias: 'EDBI',
         website: 'https://www.edbi.ir'
     },
+    '27760': {
+        name: 'Post Bank of Iran',
+        alias: 'Post Bank of Iran',
+        website: 'http://www.postbank.ir'
+    },
     '27961': {
         name: 'Bank of Industry and Mine',
         alias: 'BIM',
         website: 'http://www.bim.ir'
     },
+    '28023': {
+        name: 'Bank Maskan',
+        alias: 'Bank Maskan',
+        website: 'http://bank-maskan.ir'
+    },
+    '36214': {
+        name: 'Ayandeh Bank',
+        alias: 'BA',
+        website: 'https://www.ba24.ir'
+    },
+    '39346': {
+        name: 'Sina Bank',
+        alias: 'Sina Bank',
+        website: 'https://www.sinabank.ir'
+    },
+    '39370': {
+        name: 'Mehr Eqtesad Bank',
+        alias: 'ME Bank',
+        website: 'https://www.mebank.ir'
+    },
+    '39607': {
+        name: 'Sarmayeh Bank',
+        alias: 'Sarmayeh Bank',
+        website: 'https://www.sbank.ir'
+    },
+    '89210': {
+        name: 'Bank Sepah',
+        alias: 'Bank Sepah',
+        website: 'https://www.ebanksepah.ir'
+    },
+    '89463': {
+        name: 'Refah Bank',
+        alias: 'Refah Bank',
+        website: 'http://refah-bank.ir'
+    },
 };
-const cardTypes = {
+const types = {
     '3': 'travel',
     '5': 'bank',
     '6': 'bank' // bank or financial institution
@@ -51,5 +151,13 @@ function verify(code) {
     return sum % 10 === 0;
 }
 exports.verify = verify;
-function identify(code) { }
+function identify(code) {
+    code = code.replace(/\D/g, '');
+    let valid = verify(code);
+    return {
+        valid,
+        type: valid ? types[code[0]] : undefined,
+        bank: valid ? banks[code.slice(1, 5)] : undefined
+    };
+}
 exports.identify = identify;
